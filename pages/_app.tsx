db9151fef27d2f371a30a8c05abc6f911a855e0b
David Bouscarle <d.bouscarle@gmail.com>
2023-02-15 22:25:40 +0100

    Implement Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
+import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+import Layout from "@/components/Layout";
 import "@/styles/normalize.css";
 import "@/styles/reset.css";
-import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Component {...pageProps} />
+            <Analytics />
         </Layout>
     );
 }
