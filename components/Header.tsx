d1369142568dbb1207590c2ae3f0cf9792566772
David Bouscarle <d.bouscarle@gmail.com>
2023-03-26 18:48:27 +0200

    Add header with animated burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,63 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
+import { chakra } from '@chakra-ui/system';
+
+const AnimatedBox = chakra(motion.div);
 
 const Header: FC = () => {
+    const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+    const toggleBurger = () => {
+        setIsBurgerOpen(!isBurgerOpen);
+    };
+
     return (
-        <header>
-        </header>
+        <Box
+            as="header"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={10}
+        >
+            <Image
+                src="/david_bouscarle_logo.png"
+                alt="Logo David Bouscarle photography"
+                width={50}
+                height={50}
+            />
+            <Box 
+                position="relative" 
+                w="40px" 
+                h="16px"
+                cursor="pointer"
+                onClick={toggleBurger}
+            >
+                <AnimatedBox
+                    initial={false}
+                    animate={isBurgerOpen ? { rotate: 45, y: "7px" } : { rotate: 0 }}
+                    transition={{ duration: "0.5" }}
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    w="100%"
+                    h="2px"
+                    backgroundColor="black"
+                ></AnimatedBox>
+                <AnimatedBox
+                    initial={false}
+                    animate={isBurgerOpen ? { rotate: -45, y: "-7px" } : { rotate: 0 }}
+                    transition={{ duration: "0.5" }}
+                    position="absolute"
+                    bottom={0}
+                    left={0}
+                    w="100%"
+                    h="2px"
+                    backgroundColor="black"
+                ></AnimatedBox>
+            </Box>
+        </Box>
     );
 };
 
